4feefcd71ed9d3b4affe1fbde797bc9120e33d1f
Masum <masum2mina@gmai.com>
2021-02-01 14:08:15 +0600

    Eloquest relationship one to many many to many

--- app/Post.php
+++ app/Post.php
@@ -9,4 +9,11 @@ class Post extends Model
     protected $fillable = [
         'title', 'discription', 'status', 'user_id '
     ];
+
+    public function user(){
+    	return $this->belongsTo('App\user', 'user_id', 'id');
+    }
+    public function tags(){
+    	return $this->belongsToMany(Tag::class, 'post_tag', 'post_id', 'tag_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\User;
 use App\Post;
+use App\Post_tag;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,17 +32,32 @@ Route::get('user', 		'homeController@index')->name('user_list');
 // //Route::get('my-user', 'homeController@index')->name('my_user');
 // Route::get('create', 'UserController@create');
 
-Route::get('create_post', function(){
-	$data = [
-		'title'=>'This is my first post',
-		'discription'=>'I want to be a good programmer i may allah help to success my dream',
-		'user_id'=>1,
-		'status'=>1
-	];
 
-	$fff = Post::create($data);
-	if($fff){
-		echo 'User create successfullu'; 
+// one to one relationship
+Route::get('one-to-one', function(){
+	$user = User::find(2);
+	dd($user->address);
+});
+
+Route::get('one-to-many', function(){
+	$user = User::find(2);
+
+	//dd($user->posts[1]->title);
+	foreach ($user->posts as $value) {
+		echo $value->title;
 	}
+});
 
+Route::get('many-inverse', function(){
+	$posts = Post::find(2);
+	$posts->user->name = 'Mim Mina';
+	$posts->user->save();
+	dd($posts->user->name);
+});
+
+// many to many relationa
+Route::get('many-to-many', function(){
+	$post = Post::find(2);
+	$posttag = Post_tag::get();
+	dd($post->tags);
 });
